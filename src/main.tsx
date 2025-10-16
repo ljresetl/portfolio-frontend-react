28c7d05c488ff8cf3810ad2218cbb0fae0eeaf97
jres <ljresetl@gmail.com>
2025-10-16 22:50:38 +0200

    helmet

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { HelmetProvider } from "react-helmet-async"; // метаопис
 import App from './components/App/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* ✅ Обгортаємо весь додаток HelmetProvider */}
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
