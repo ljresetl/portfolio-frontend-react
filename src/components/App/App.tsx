c3752981cd593d548ef2cc1a5e610f5169b343a9
jres <ljresetl@gmail.com>
2025-10-16 18:11:30 +0200

    heder

---
+++ src/components/App/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from '../Header/Header'
+// import Foto from '../Foto/Foto'
+// import AboutMe from '../About-me/AboutMe'
+// import Capabilities from '../Capabilities/Capabilities'
+// import Experience from '../Experience/Experience'
+// import Portfolio from '../Portfolio/Portfolio'
+// import Connect from '../Connect/Connect'
+// import Footer from '../Footer/Footer'
+
+import '../App/App.module.scss'
+
+const App: React.FC = () => {
+    return (
+        <>
+            <Header />
+            {/* <AboutMe />
+            <Foto />
+            <Capabilities />
+            <Experience />
+            <Portfolio />
+            <Connect />
+            <Footer /> */}
+        </>
+    )
+}
+
+export default App
