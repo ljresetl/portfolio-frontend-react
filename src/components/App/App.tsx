133453d96e2ffda6689213e73a852f5520256586
jres <ljresetl@gmail.com>
2025-10-17 23:11:51 +0200

    index.html-oprava

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from '../Header/Header';
 import AboutMe from '../About-me/AboutMe';
 import Foto from '../Foto/Foto';
@@ -11,26 +11,6 @@ import Footer from '../Footer/Footer';
 import '../App/App.module.scss';
 
 const App: React.FC = () => {
-  // Додаємо title і meta description динамічно
-  useEffect(() => {
-    // Змінюємо заголовок сторінки
-    document.title = "Моє портфоліо FrontEnd розробника";
-
-    // Шукаємо meta description
-    const metaDescription = document.querySelector('meta[name="description"]');
-
-    if (metaDescription) {
-      // Якщо тег існує — оновлюємо content
-      metaDescription.setAttribute("content", "Приклади моїх проектів FrontEnd, портфоліо з React, HTML, CSS, JS та TypeScript.");
-    } else {
-      // Якщо тегу немає — створюємо його
-      const meta = document.createElement("meta");
-      meta.name = "description";
-      meta.content = "Приклади моїх проектів FrontEnd, портфоліо з React, HTML, CSS, JS та TypeScript.";
-      document.head.appendChild(meta);
-    }
-  }, []);
-
   return (
     <>
       <Header />
