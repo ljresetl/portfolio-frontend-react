1697f862abbc0e23a1d3302496dc2dc23001944c
jres <ljresetl@gmail.com>
2025-10-18 13:04:13 +0200

    portfolio-768

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -12,52 +12,54 @@ const Portfolio: React.FC = () => {
   return (
     <section id="portfolio" className={styles.portfolio}>
       <div className={styles.container}>
-        <h3 className={styles.portfolio_h}>Приклади робіт</h3>
-
-        {/* Рендер карток через map */}
-        <div className={styles.portfolio_list}>
-          {projects.slice(0, visibleCount).map((project, index) => (
-            <div key={index} className={styles.project_card}>
-              <h4 className={styles.portfolio_h_4}>{project.title}</h4>
-              <p className={styles.portfolio_p}>{project.description}</p>
-              <img
-                src={project.image}
-                alt={project.title}
-                width={300}
-                height={310}
-                className={styles.project_image}
-              />
-              <p className={styles.portfolio_p}>Технології:</p>
-              <ul className={styles.project_tech}>
-                {project.technologies.map((tech, i) => (
-                  <li key={i}>{tech}</li>
-                ))}
-              </ul>
-              <div className={styles.project_links}>
-                      <a href={project.github}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.project_button}>
-                  GitHub
-                </a>
-                      <a href={project.website}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.project_button}>
-                  Вебсайт
-                </a>
+       <div className={styles.content}>
+          <h3 className={styles.portfolio_h}>Приклади робіт</h3>
+  
+          {/* Рендер карток через map */}
+          <div className={styles.portfolio_list}>
+            {projects.slice(0, visibleCount).map((project, index) => (
+              <div key={index} className={styles.project_card}>
+                <h4 className={styles.portfolio_h_4}>{project.title}</h4>
+                <p className={styles.portfolio_p}>{project.description}</p>
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  width={300}
+                  height={310}
+                  className={styles.project_image}
+                />
+                <p className={styles.portfolio_p_t}>Технології:</p>
+                <ul className={styles.project_tech}>
+                  {project.technologies.map((tech, i) => (
+                    <li key={i}>{tech}</li>
+                  ))}
+                </ul>
+                <div className={styles.project_links}>
+                        <a href={project.github}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className={styles.project_button}>
+                    GitHub
+                  </a>
+                        <a href={project.website}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className={styles.project_button}>
+                    Вебсайт
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+  
+          {/* Кнопка "Показати більше" */}
+          {visibleCount < projects.length && (
+            <button className={styles.button_loadmore} onClick={loadMore}>
+              Показати більше
+            </button>
+          )}
         </div>
-
-        {/* Кнопка "Показати більше" */}
-        {visibleCount < projects.length && (
-          <button className={styles.button_loadmore} onClick={loadMore}>
-            Показати більше
-          </button>
-        )}
-      </div>
+       </div>
     </section>
   );
 };
